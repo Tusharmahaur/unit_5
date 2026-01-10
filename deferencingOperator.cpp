e4582f58b8e37fc518c94373fc2271bb7951dc70
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-10 15:38:04 +0530

    this

---
+++ deferencingOperator.cpp
@@ -0,0 +1,82 @@
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class M{
+//     int x,y;
+//     public: 
+//     void set_xy(int a,int b){
+//         x = a;
+//         y = b;
+//     }
+//     friend int sum(M m);
+
+// };
+
+// int sum(M m){
+//     int M ::* px = &M::x;
+//     int M ::* py = &M ::y;
+//     M *pm = &m;
+//     int S = m.*px + pm->*py;
+//     return S;
+// }
+// int main(){
+//     M n;
+//     void(M:: *pf)(int ,int) = &M :: set_xy;
+//     (n.*pf)(10,20);
+//     cout<<"Sum = "<<sum(n)<<endl;
+//     M*op = &n;
+//     (op->*pf)(30,40);
+//     cout<<"Sum = "<<sum(n)<<endl;
+    
+//     return 0;
+// }
+
+
+//pointer to pointer address 
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int a = 3,b=4;
+    
+//     int *ptr = &a;//pointer store to address 
+
+//     int **parentPtr = &ptr;//pointer to pointer address store
+
+//     int ***ppPtr = &parentPtr;//pointer to pointer to pointer address store 
+
+//     int ****pppPtr = &ppPtr;//pointer to pointer to pointer to pointer address store in four times
+
+//     cout<<&ptr<<endl;
+//     cout<<parentPtr<<endl;
+//     cout<<&parentPtr<<endl;
+//     cout<<ppPtr<<endl;
+//     cout<<&ppPtr<<endl;
+//     cout<<pppPtr<<endl;
+    
+//     return 0;
+// }
+
+//dereferencing operator -> ka matlab sirf itna hai ki value ko print kara dena jo uss address par hai 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 3,b=4;
+    
+    int *ptr = &a;//pointer store to address 
+
+    int **parentPtr = &ptr;//pointer to pointer address store
+
+    int ***ppPtr = &parentPtr;//pointer to pointer to pointer address store 
+
+    int ****pppPtr = &ppPtr;//pointer to pointer to pointer to pointer address store in four times
+
+    <<*(&a)<<endl       // |
+    cout<<a<<endl       // | -> Same value print is 3
+        <<*ptr<<endl;   // |
+    
+    return 0;
+}
